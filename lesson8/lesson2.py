2a7889b9d4f1c1c4658d1d5cdda2898c0de546de
userlesson <motorus1@gmail.com>
2026-02-17 19:25:30 +0300

    Add files via upload

--- lesson8/lesson2.py
+++ lesson8/lesson2.py
@@ -1,7 +1,10 @@
+print("Введите размер списка")
 N = int(input())
 
 arr = list(map(int, input().split()))
 
-res = [arr[-1]] + arr[:-1]
-
-print(res)
+if len(arr) > N:
+    print("Размер списка превышает указанный")
+else:
+    res = [arr[-1]] + arr[:-1]
+    print(res)
